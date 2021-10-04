24ba860565e10d5ab3bee222726c0b7d23706f6f
rosani <rsn.smn@gmail.com>
2021-10-04 12:33:37 +0200

    Added static typing and setup.cfg file

--- bowling_game/game.py
+++ bowling_game/game.py
@@ -2,29 +2,29 @@ import random
 
 
 class Game:
-    frames = {}
-    pins_count = 10
+    frames: dict = {}
+    pins_count: int = 10
 
-    def scoreboard_generator(self):
+    def scoreboard_generator(self) -> None:
         frame_count = 1
         while frame_count <= 10:
             self.frame(frame_count, self.pins_count)
             frame_count += 1
 
-    def frame(self, frame_count, pins_count):
+    def frame(self, frame_count: int, pins_count: int) -> None:
         round_count = 1
         self.frames[f"frame_number_{frame_count}"] = {}
         while round_count <= 2:
             self.round(frame_count, round_count, pins_count)
             round_count += 1
 
-    def round(self, frame_count, round_count, pins_count):
+    def round(self, frame_count: int, round_count: int, pins_count: int) -> None:
         roll_result = self.roll(pins_count)
         self.frames[f"frame_number_{frame_count}"].update(
             {f"roll_{round_count}": roll_result}
         )
 
-    def roll(self, pins_count):
+    def roll(self, pins_count: int) -> int:
         pins_hit = random.randint(0, pins_count)
         pins_count -= pins_hit
         return pins_hit
