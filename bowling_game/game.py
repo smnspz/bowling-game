c661323906e1db33bc11ef13d6cd2e13de707060
rosani <rsn.smn@gmail.com>
2021-10-04 14:08:01 +0200

    Finished implementing frames system

--- bowling_game/game.py
+++ bowling_game/game.py
@@ -3,28 +3,33 @@ import random
 
 class Game:
     frames: dict = {}
-    pins_count: int = 10
 
     def scoreboard_generator(self) -> None:
         frame_count = 1
         while frame_count <= 10:
-            self.frame(frame_count, self.pins_count)
+            pins_count = 10
+            self.frame(frame_count, pins_count)
             frame_count += 1
+        self.score(self.frames)
 
-    def frame(self, frame_count: int, pins_count: int) -> None:
+    def frame(self, frame_count: int, pins_count: int) -> int:
         round_count = 1
         self.frames[f"frame_number_{frame_count}"] = {}
         while round_count <= 2:
-            self.round(frame_count, round_count, pins_count)
+            pins_count = self.round(frame_count, round_count, pins_count)
             round_count += 1
+        return pins_count
 
-    def round(self, frame_count: int, round_count: int, pins_count: int) -> None:
+    def round(self, frame_count: int, round_count: int, pins_count: int) -> int:
         roll_result = self.roll(pins_count)
         self.frames[f"frame_number_{frame_count}"].update(
             {f"roll_{round_count}": roll_result}
         )
+        pins_count -= roll_result
+        return pins_count
 
     def roll(self, pins_count: int) -> int:
-        pins_hit = random.randint(0, pins_count)
-        pins_count -= pins_hit
-        return pins_hit
+        return random.randint(0, pins_count)
+
+    def score(self, frames):
+        print(frames)
